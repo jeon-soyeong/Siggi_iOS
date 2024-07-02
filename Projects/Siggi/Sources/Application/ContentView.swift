62f009f706c9de7caa52988b1c79e0b02bdca732
전소영 <jsu3417@gmail.com>
2024-07-02 23:10:54 +0900

    [#1] TabView 로직 추가

--- Projects/Siggi/Sources/Application/ContentView.swift
+++ Projects/Siggi/Sources/Application/ContentView.swift
@@ -10,14 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(AppRootManager.self) private var appRootManger
+    @State private var selectedTab: Tab = .search
     
     var body: some View {
         switch appRootManger.currentRoot {
         case .splash:
             SplashView()
-        case .search:
-            SearchView()
         case .home:
+            TabView(selection: $selectedTab) {
+                NavigationStack {
+                    SearchView()
+                }
+                .tabItem {
+                    Label("검색", systemImage: "magnifyingglass")
+                }
+                .tag(Tab.search)
+                
+                NavigationStack {
+                    RecordView()
+                }
+                .tabItem {
+                    Label("기록", systemImage: "pencil.tip")
+                }
+                .tag(Tab.record)
+            }
+            .accentColor(.red)
+            .onAppear(perform: {
+                UITabBar.appearance().unselectedItemTintColor = .darkGray
+            })
         }
     }
 }
