4dbabdc970b45d6b7cf2c91444bc5e4400916e8f
전소영 <jsu3417@gmail.com>
2024-07-02 23:07:17 +0900

    [#1] RecordView 추가

---
+++ Projects/Siggi/Sources/Presentation/Record/RecordView.swift
@@ -0,0 +1,18 @@
+//
+//  RecordView.swift
+//  Siggi
+//
+//  Created by 전소영 on 2024/07/02.
+//
+
+import SwiftUI
+
+struct RecordView: View {
+    var body: some View {
+        Text("RecordView!")
+    }
+}
+
+#Preview {
+    RecordView()
+}
