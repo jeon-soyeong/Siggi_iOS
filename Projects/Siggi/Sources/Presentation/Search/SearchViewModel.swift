fe56590ef5df76c968275e146d8f8c380f8231ca
전소영 <jsu3417@gmail.com>
2024-09-25 22:53:34 +0900

    [#9] SearchViewModel searchButtonTapped 로직 구현

--- Projects/Siggi/Sources/Presentation/Search/SearchViewModel.swift
+++ Projects/Siggi/Sources/Presentation/Search/SearchViewModel.swift
@@ -7,22 +7,35 @@
 
 import SwiftUI
 
-@Observable public final class SearchViewModel {
+@Observable public final class SearchViewModel: ViewModelType {
     public enum Action {
-        
+        case searchButtonTapped(searchText: String)
     }
-    
-    public struct State {
 
+    public struct State {
+        var searchPlaceResults: SearchPlaces?
+        var isEnd: Bool = false
     }
-    
-    public var state: State
-    
-    public init() {
-        self.state = State()
-    }
-    
+
+    public var state = State()
+
     public func transform(type: Action) {
-        
+        Task {
+            switch type {
+            case .searchButtonTapped(let searchText):
+                try await fetchSearchPlaceResults(searchText: searchText)
+            }
+        }
+    }
+
+    @MainActor
+    public func fetchSearchPlaceResults(searchText: String) async throws {
+        guard let request = URLRequest(type: SearchAPI.searchPlace(query: searchText, page: 1, size: 15)) else {
+            return
+        }
+        // TODO: UseCase로 변경
+        let searchPlace: SearchPlaces = try await APIService().request(with: request)
+        print("searchPlace: \(searchPlace)")
+        state.searchPlaceResults = searchPlace
     }
 }
