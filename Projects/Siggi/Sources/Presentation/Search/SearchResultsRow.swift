b0e3bd12720e290d201ad895da59368d465941ee
전소영 <jsu3417@gmail.com>
2024-10-10 15:39:22 +0900

    [#9] SearchResultsRow roadAddressName 	추가

--- Projects/Siggi/Sources/Presentation/Search/SearchResultsRow.swift
+++ Projects/Siggi/Sources/Presentation/Search/SearchResultsRow.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 public struct SearchResultsRow: View {
-    var place: String
-    
+    var place: Document
+
     public var body: some View {
-        HStack {
+        HStack(alignment: .top) {
             Image(.place)
                 .resizable()
                 .frame(width: 18, height: 16)
-            
-            Text(place)
+                .padding(.top, 2)
+
+            VStack(alignment: .leading, spacing: 5) {
+                Text(place.placeName)
+                Text(place.roadAddressName)
+                    .font(.system(size: 14))
+                    .foregroundStyle(.gray)
+            }
         }
-        .padding(EdgeInsets(top: 8, leading: 14, bottom: 8, trailing: 0))
+        .padding(EdgeInsets(top: 6, leading: 14, bottom: 6, trailing: 0))
         Divider()
     }
 }
 
 #Preview {
-    SearchResultsRow(place: "")
+    SearchResultsRow(place: Document(placeName: "미가훠궈양고기", x: "127.09820149830225", y: "37.32271855940384", roadAddressName: "경기 용인시 수지구 풍덕천로140번길 15", phone: "031-272-2222"))
 }
