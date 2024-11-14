ac86bd5b792776a767f849460705fd9d3982689a
전소영 <jsu3417@gmail.com>
2024-11-14 15:23:16 +0900

    [#13] UIImage+Extension 추가

---
+++ Projects/Siggi/Sources/Utils/Extension/UIImage+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  UIImage+Extension.swift
+//  Siggi
+//
+//  Created by 전소영 on 2024/10/29.
+//
+
+import UIKit
+import CryptoKit
+
+extension UIImage {
+    func hashValue() -> String? {
+        guard let data = self.pngData() else { return nil }
+        let hash = SHA256.hash(data: data)
+        return hash.compactMap { String(format: "%02x", $0) }.joined()
+    }
+}
