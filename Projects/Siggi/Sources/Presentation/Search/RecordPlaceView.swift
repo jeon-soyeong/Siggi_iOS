544c8c40b3be9df96fdae203ac6223e3e98c3ca9
전소영 <jsu3417@gmail.com>
2024-12-10 23:02:13 +0900

    [#13] isLoading, isRegister 변경

--- Projects/Siggi/Sources/Presentation/Search/RecordPlaceView.swift
+++ Projects/Siggi/Sources/Presentation/Search/RecordPlaceView.swift
@@ -5,18 +5,22 @@
 //  Created by 전소영 on 2024/10/23.
 //
 
-import SwiftUI
 import Common
 import PhotosUI
+import SwiftData
+import SwiftUI
 
 struct RecordPlaceView: View {
     @Environment(Router.self) private var searchRouter
-    var place: Document?
-    private let maximumRating: Int = 5
+    @Environment(\.modelContext) private var modelContext
+    @State private var selectedImages: [UIImage] = []
     @State private var rating: Int = 0
-    @State private var isLoading: Bool = false
     @State private var text: String = ""
+    @State private var isLoading: Bool = false
+    @State private var isRegister: Bool = false
     @FocusState private var isFocused: Bool
+    private let maximumRating: Int = 5
+    var place: Document?
 
     var body: some View {
         ZStack {
@@ -25,67 +29,96 @@ struct RecordPlaceView: View {
                     NavigationBar(title: place.placeName,
                                   backButtonAction: { searchRouter.popView() },
                                   rightButtonAction: { searchRouter.popToRootView() })
+                    Divider()
 
-                Divider()
-
-                ScrollView {
-                    HStack {
-                        ForEach(1...maximumRating, id: \.self) { count in
-                            Image(systemName: count > rating ? "star" : "star.fill")
-                                .resizable()
-                                .frame(width: 30, height: 30)
-                                .foregroundColor(count > rating ? .gray : .red)
-                                .onTapGesture {
-                                    rating = count
-                                }
+                    ScrollView {
+                        HStack {
+                            ForEach(1...maximumRating, id: \.self) { count in
+                                Image(systemName: count > rating ? "star" : "star.fill")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundColor(count > rating ? .gray : .red)
+                                    .onTapGesture {
+                                        rating = count
+                                    }
+                            }
                         }
-                    }
-                    .padding(30)
+                        .padding(30)
 
-                    PhotoView(isLoading: $isLoading)
-                        .padding(20)
+                        PhotoView(selectedImages: $selectedImages, isLoading: $isLoading)
+                            .padding(20)
 
-                    VStack(alignment: .trailing) {
-                        TextField("리뷰를 작성해주세요 🖊️", text: $text, axis: .vertical)
-                            .focused($isFocused)
-                            .lineLimit(9...)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(Color.gray, lineWidth: 1)
-                                    .padding(-10)
-                            )
-                            .padding(.horizontal, 30)
-                            .onChange(of: text) { oldValue, newValue in
-                                if newValue.count > 500 {
-                                    text = String(newValue.prefix(500))
+                        VStack(alignment: .trailing) {
+                            TextField("리뷰를 작성해주세요 🖊️", text: $text, axis: .vertical)
+                                .focused($isFocused)
+                                .lineLimit(9...)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.gray, lineWidth: 1)
+                                        .padding(-10)
+                                )
+                                .padding(.horizontal, 30)
+                                .onChange(of: text) { oldValue, newValue in
+                                    if newValue.count > 500 {
+                                        text = String(newValue.prefix(500))
+                                    }
+                                }
+
+                            Text("\(text.count) / 500")
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                                .padding(.top, 10)
+                                .padding(.trailing, 20)
+                        }
+                        .padding(.top, 20)
+
+                        Image(.save)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .padding(EdgeInsets(top: 30, leading: 18, bottom: 0, trailing: 18))
+                            .onTapGesture {
+                                isRegister = true
+
+                                Task {
+                                    var selectedImageDatas: [Data] = []
+                                    for selectedImage in selectedImages {
+                                        if let selectedImageData = selectedImage.pngData() {
+                                            selectedImageDatas.append(selectedImageData)
+                                        }
+                                    }
+
+                                    guard let latitude = Double(place.y),
+                                          let longitude = Double(place.x) else {
+                                        isRegister = false
+                                        return
+                                    }
+
+                                    do {
+                                        modelContext.insert(PlaceRecord(name: place.placeName, latitude: latitude, longitude: longitude, rating: rating, imageData: selectedImageDatas, text: text))
+                                        try modelContext.save()
+                                        isRegister = false
+                                        searchRouter.popToRootView()
+                                    } catch {
+                                        isRegister = false
+                                        print("Failed to save context: \(error)")
+                                    }
                                 }
                             }
-
-                        Text("\(text.count) / 500")
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                            .padding(.top, 10)
-                            .padding(.trailing, 20)
                     }
-                    .padding(.top, 20)
-
-                    Image(.save)
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .padding(EdgeInsets(top: 30, leading: 18, bottom: 0, trailing: 18))
-                        .onTapGesture {
-                            searchRouter.popToRootView()
-                        }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        isFocused = false
+                    }
+                    .scrollIndicators(.hidden)
                 }
-                .contentShape(Rectangle())
-                .onTapGesture {
-                    isFocused = false
-                }
-                .scrollIndicators(.hidden)
             }
 
             if isLoading {
-                ProgressView()
+                // 팝업
+            }
+
+            if isRegister {
+                // 팝업
             }
         }
     }
@@ -97,10 +130,9 @@ struct RecordPlaceView: View {
 
 struct PhotoView: View {
     @State private var selectedItems: [PhotosPickerItem] = []
-    @State private var selectedImages: [UIImage] = []
-    @State private var selectedImagesHashes: Set<String> = []
-    @State private var previousSelectedItems: [PhotosPickerItem] = []
+    @Binding var selectedImages: [UIImage]
     @Binding var isLoading: Bool
+    @State var isRemove: Bool = false
 
     var body: some View {
         HStack {
@@ -125,35 +157,20 @@ struct PhotoView: View {
                 }
                 .onChange(of: selectedItems) {
                     Task {
-                        if previousSelectedItems.count < selectedItems.count {
+                        if !isRemove {
                             isLoading = true
                             defer { isLoading = false }
 
+                            selectedImages = []
                             for selectedItem in selectedItems {
                                 if let data = try? await selectedItem.loadTransferable(type: Data.self),
-                                   let image = UIImage(data: data),
-                                   let imageHash = image.hashValue() {
-
-                                    if !selectedImagesHashes.contains(imageHash) {
-                                        selectedImages.append(image)
-                                        selectedImagesHashes.insert(imageHash)
-                                    }
+                                   let image = UIImage(data: data) {
+                                    selectedImages.append(image)
                                 }
                             }
+                        } else {
+                            isRemove = false
                         }
-
-                        let removedItems = previousSelectedItems.filter { !selectedItems.contains($0) }
-                        for removedItem in removedItems {
-                            if let data = try? await removedItem.loadTransferable(type: Data.self),
-                               let image = UIImage(data: data),
-                               let imageHash = image.hashValue() {
-                                if let index = selectedImages.firstIndex(where: { $0.hashValue() == imageHash }) {
-                                    selectedImages.remove(at: index)
-                                    selectedImagesHashes.remove(imageHash)
-                                }
-                            }
-                        }
-                        previousSelectedItems = selectedItems
                     }
                 }
 
@@ -169,27 +186,15 @@ struct PhotoView: View {
                                 .clipped()
 
                             Button(action: {
-                                guard !isLoading else { return }
-
-                                isLoading = true
-                                DispatchQueue.main.async {
-                                    let removedImage = selectedImages[index]
-
-                                    if let removedImageHash = removedImage.hashValue() {
-                                        selectedImagesHashes.remove(removedImageHash)
-                                    }
-
-                                    selectedImages.remove(at: index)
-                                    selectedItems.remove(at: index)
-                                    isLoading = false
-                                }
+                                isRemove = true
+                                selectedImages.remove(at: index)
+                                selectedItems.remove(at: index)
                             }, label: {
                                 Image(.remove)
                                     .resizable()
                                     .frame(width: 20, height: 20)
                                     .padding(5)
                             })
-                            .disabled(isLoading)
                         }
                     }
                 }
