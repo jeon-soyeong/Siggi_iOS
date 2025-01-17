a8c5940cafd18d55f949704eaf80da5f97f5976a
전소영 <jsu3417@gmail.com>
2025-01-17 22:54:55 +0900

    [#17] PlaceRecordsView UI, PopUpView 추가

--- Projects/Siggi/Sources/Presentation/Search/PlaceRecordsView.swift
+++ Projects/Siggi/Sources/Presentation/Search/PlaceRecordsView.swift
@@ -5,10 +5,16 @@
 //  Created by 전소영 on 2025/01/08.
 //
 
+import Common
+import SwiftData
 import SwiftUI
 
 struct PlaceRecordsView: View {
-    @Binding var record: [PlaceRecord]?
+    @Query(sort: \PlaceRecord.date, order: .reverse) var placeRecords: [PlaceRecord]
+    @Binding var placeName: String?
+    @State private var isDelete: Bool = false
+    @State private var recordToDelete: PlaceRecord?
+    @Environment(\.modelContext) private var modelContext
     private let maximumRating: Int = 5
     private let dateFormatter: DateFormatter = {
         let formatter = DateFormatter()
@@ -18,29 +24,40 @@ struct PlaceRecordsView: View {
     }()
 
     var body: some View {
-        if let record = record,
-           let placeName = record.first?.name {
             ZStack(alignment: .top) {
-                VStack(alignment: .leading) {
-                    Text(placeName)
-                        .font(.title)
-                        .lineLimit(1)
-                        .truncationMode(.tail)
-                        .padding(.top, 30)
+                if let placeName = placeName {
+                    VStack(alignment: .leading) {
+                        Text(placeName)
+                            .font(.title)
+                            .lineLimit(1)
+                            .truncationMode(.tail)
+                            .padding(.top, 30)
 
-                    Divider()
+                        Divider()
+                    }
+                    .background(.white)
+                    .zIndex(1)
                 }
-                .background(.white)
-                .zIndex(1)
 
                 ScrollView {
-                    ForEach(record.indices, id: \.self) { index in
+                    ForEach(placeRecords.filter { $0.name == placeName }, id: \.self) { placeRecord in
                         VStack(alignment: .leading) {
-                            let placeRecord = record[index]
+                            HStack {
+                                let formattedDate = dateFormatter.string(from: placeRecord.date)
+                                Text(formattedDate)
+                                    .fontWeight(.medium)
 
-                            let formattedDate = dateFormatter.string(from: placeRecord.date)
-                            Text(formattedDate)
-                                .fontWeight(.medium)
+                                Spacer()
+
+                                Button(action: {
+                                    recordToDelete = placeRecord
+                                    isDelete = true
+                                }, label: {
+                                    Image(.trashCan)
+                                        .resizable()
+                                        .frame(width: 18, height: 15)
+                                })
+                            }
 
                             HStack(spacing: 2) {
                                 ForEach(1...maximumRating, id: \.self) { count in
@@ -81,9 +98,31 @@ struct PlaceRecordsView: View {
                 }
                 .scrollIndicators(.hidden)
                 .safeAreaPadding(EdgeInsets(top: 90, leading: 0, bottom: 0, trailing: 0))
+
+                if isDelete, let placeRecordToDelete = recordToDelete {
+                    PopUpView(message: "정말로 삭제하시겠습니까?",
+                              leftButtonAction: {
+                                  do {
+                                      modelContext.delete(placeRecordToDelete)
+                                      try modelContext.save()
+                                      isDelete = false
+                                      recordToDelete = nil
+                                  } catch {
+                                      print("Failed to delete: \(error)")
+                                  }
+                              },
+                              rightButtonAction: {
+                                  isDelete = false
+                                  recordToDelete = nil
+                              },
+                              leftButtonImageName: "yes",
+                              rightButtonImageName: "no",
+                              height: 250
+                    )
+                    .zIndex(2)
+                }
             }
             .frame(maxWidth: .infinity, alignment: .leading)
             .padding()
-        }
     }
 }
