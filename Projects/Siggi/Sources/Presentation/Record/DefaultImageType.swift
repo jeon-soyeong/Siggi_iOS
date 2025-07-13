c6c506887a2ad1a88150ac039eb58844817771f4
전소영 <jsu3417@gmail.com>
2025-07-13 17:23:34 +0900

    [#22] enum DefaultImageType 추가

---
+++ Projects/Siggi/Sources/Presentation/Record/DefaultImageType.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultImageType.swift
+//  Siggi
+//
+//  Created by 전소영 on 2025/07/12.
+//
+
+import Foundation
+
+enum DefaultImageType {
+    case asset(String)
+    case systemSymbol(String)
+}
